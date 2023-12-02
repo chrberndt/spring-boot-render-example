f571d1687c773288c3795849f6b8fbe1ca88a71e
Christian Berndt <christian.berndt@liferay.com>
2023-12-02 23:07:27 +0100

    Initial check-in.

---
+++ src/main/java/com/chberndt/demo/controller/CustomerController.java
@@ -0,0 +1,25 @@
+package com.chberndt.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chberndt.demo.model.Customer;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/customers")
+public class CustomerController {
+    private static final List<Customer> CUSTOMERS = List.of(
+            new Customer(1L, "john", "doe", "john@chberndt.com"),
+            new Customer(2L, "mary", "public", "mary@chberndt.com"),
+            new Customer(3L, "elon", "musk","elon@chberndt.com"),
+            new Customer(4L, "dunny","duncan","dunny@chberndt.com")
+    );
+
+    @GetMapping
+    public List<Customer> findAllCustomers(){
+        return CUSTOMERS;
+    }
+}
